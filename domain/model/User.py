afebcead58457025383b2b61a28daa3cf4c1f0e1
Cesar David Millan <cesardavidmillan5@gmail.com>
2024-06-02 20:01:09 -0500

    Initial Commit Clean Architecture Structure With Python

---
+++ domain/model/User.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self, id, name, email, password):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.password = password
+
+    def __str__(self):
+        return f'User(id={self.id}, name={self.name}, email={self.email}, password={self.password})'
